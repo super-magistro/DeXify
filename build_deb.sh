c709020075d26a4fb9da0026a2eced6fbf9955cd
romain_guillon <romain.guillon@camileia.com>
2026-08-10 14:02:19 +0200

    [FIX] Match ^__version__ strictly in build_deb.sh to avoid capturing v{__version__} UI string

--- build_deb.sh
+++ build_deb.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ -f "dextop.py" ]; then
-    VERSION=$(grep '__version__' dextop.py | cut -d'"' -f2)
+    VERSION=$(grep '^__version__' dextop.py | cut -d'"' -f2)
 else
     VERSION="1.0.20"
 fi
